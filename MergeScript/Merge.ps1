1e0744ed45601cab881be72c0f67bd4fa1c7f4c4
goliver1984 <goliver1984@gmail.com>
2021-05-10 15:21:03 +0100

    Updated to include merge script

---
+++ MergeScript/Merge.ps1
@@ -0,0 +1,24 @@
+
+$dirstomerge = Get-ChildItem In -Directory
+
+foreach($dir in $dirstomerge)
+{
+   
+   $filestomerge = Get-ChildItem In\$dir -File
+
+   foreach($file in $filestomerge)
+   {
+     if ($file.Extension -eq ".txt")
+     {
+        if (-Not (Test-Path Out\$file))
+        { 
+            New-Item Out\$file -Force
+        }
+
+        write-host $file
+    
+        Add-Content Out\$file -value (Get-Content In\$dir\$File)
+     }
+   }
+
+}
